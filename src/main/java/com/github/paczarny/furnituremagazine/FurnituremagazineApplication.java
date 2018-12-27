03d908e19ab79a777599e9f769d21fa61860e9ee
paczarny <pawel350812@gmail.com>
2018-12-27 22:01:33 +0100

    initial commit

---
+++ src/main/java/com/github/paczarny/furnituremagazine/FurnituremagazineApplication.java
@@ -0,0 +1,14 @@
+package com.github.paczarny.furnituremagazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnituremagazineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurnituremagazineApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/github/paczarny/furnituremagazine/FurnituremagazineApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.paczarny.furnituremagazine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FurnituremagazineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
