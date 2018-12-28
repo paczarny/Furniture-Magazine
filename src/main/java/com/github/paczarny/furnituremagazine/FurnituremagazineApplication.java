93b15542c89344bc0f7372c07fd7137df29cfbd5
paczarny <pawel350812@gmail.com>
2018-12-28 21:17:03 +0100

    crud methods added

--- src/main/java/com/github/paczarny/furnituremagazine/FurnituremagazineApplication.java
+++ src/main/java/com/github/paczarny/furnituremagazine/FurnituremagazineApplication.java
@@ -21,11 +21,9 @@ public class FurnituremagazineApplication {
         Address address1 = new Address("Cracow", "30-300", "Poland", "1", "1", "1");
         Address address2 = new Address("Warsaw", "30-300", "Poland", "1", "1", "1");
         AddressDao addressDao = ctx.getBean(AddressDao.class);
-        //addressDao.save(address1);
+        addressDao.save(address1);
         addressDao.save(address2);
-
-        address1 = addressDao.get(address1);
-
+        
 
         List<Address> result= addressDao.getAll();
         for(int i=0;i<result.size();i++){
