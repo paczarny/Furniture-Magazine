08d97922ffa3b1fa2c904ad399f5997eda566825
kamex737 <kamex737@gmail.com>
2019-01-05 20:09:12 +0100

    impl creating magazines

--- src/main/java/com/github/paczarny/furnituremagazine/controller/MagazineController.java
+++ src/main/java/com/github/paczarny/furnituremagazine/controller/MagazineController.java
@@ -5,6 +5,8 @@ import com.github.paczarny.furnituremagazine.domain.Magazine;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,6 +24,13 @@ public class MagazineController {
     public String listAllMagazines(Model model) {
         List<Magazine> allMagazines = magazineDao.getAll();
         model.addAttribute("allMagazines", allMagazines);
+        model.addAttribute("newMagazine", new Magazine());
         return "magazinesList";
+    }
+
+    @PostMapping
+    public String addMagazine(@ModelAttribute Magazine magazine) {
+        magazineDao.save(magazine);
+        return "redirect:/magazine/list";
     }
 }
